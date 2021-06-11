0349126ddb2470c4e02f496b50efd945b73cf819
shiro <berkelium777@gmail.com>
2021-06-11 14:37:47 -0600

    6/11/2021 02 commit GIS To AERMOD tool

--- src/runme.py
+++ src/runme.py
@@ -3,7 +3,7 @@ import logging
 from ui import MainWindow
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     logging.info('Start GIS To AERMOD Tool')
 
     root = MainWindow()
